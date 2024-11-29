9e7947d2f77aaf7831d440175266cdca370495f2
Javier Candia <javi-candia@hotmail.com>
2024-11-29 14:33:24 -0600

    Migrated CV Scanner functionality from Streamlit to Flask

--- app.py
+++ app.py
@@ -1,66 +1,84 @@
 import os
+import json
 from flask import Flask, request, jsonify
 from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains.question_answering import load_qa_chain
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+
+from langchain_openai import ChatOpenAI
+from langchain.schema import Document
+from langchain_core.prompts import PromptTemplate
+
+from output_parsers import feedback_parser
 
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
-knowledge_base = None
-
-# Initialize global embeddings model
-embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
 
 # Preinitialize the LLM
-llm = ChatOpenAI(model_name='gpt-3.5-turbo')
+llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
 
-# This chain coordinates the LLM for the specific QA task
-qa_chain = load_qa_chain(llm, chain_type="stuff")
+# Load roles from JSON file
+with open("roles-light.json", "r", encoding="utf-8") as file:
+    roles = json.load(file)
 
-@app.route('/upload', methods=['POST'])
+def create_document(pdf):
+    pdf_reader = PdfReader(pdf)
+    text = "".join(page.extract_text() for page in pdf_reader.pages)
+
+    # Filter content: remove blank lines
+    text = "\n".join(line for line in text.split("\n") if line.strip())
+
+    # Create a Document instance with the full text
+    document = Document(page_content=text, metadata={})
+    return [document]
+
+@app.route("/upload", methods=["POST"])
 def upload_pdf():
-    global knowledge_base
-    if 'pdf' not in request.files:
-        return jsonify({'error': 'No PDF uploaded'}), 400
+    if "pdf" not in request.files:
+        return jsonify({"error": "No PDF uploaded"}), 400
 
-    pdf_file = request.files['pdf']
-    pdf_reader = PdfReader(pdf_file)
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
+    pdf_file = request.files["pdf"]
+    documents = create_document(pdf_file)
 
-    # Filter out blank lines from the text
-    text = "\n".join([line for line in text.split("\n") if line.strip() != ""])
+    if roles:
+        os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-    # Split text into smaller chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
-    chunks = text_splitter.split_text(text)
+        new_match_prompt = PromptTemplate(
+            input_variables=["documents", "roles"],
+            partial_variables={
+                "format_instructions": feedback_parser.get_format_instructions
+            },
+            template="""
+                Based on the provided CV information: {documents}
+                and the following roles: {roles},
 
-    # Use the pre-initialized embeddings model to create a knowledge base
-    knowledge_base = FAISS.from_texts(chunks, embedding_model)
+                Please perform the following tasks:
 
-    return jsonify({'success': True, 'message': 'PDF processed successfully'})
+                1. *Understand the Candidate CV*
+                - Determine the candidate's level based on their overall experience: Junior, Mid or Senior.
 
-@app.route('/query', methods=['POST'])
-def query_pdf():
-    global knowledge_base
-    if not knowledge_base:
-        return jsonify({'error': 'Knowledge base not initialized'}), 400
+                2. **List the Candidate's Main Skills and Experiences**:
+                - Identify and enumerate the 5 primary skills and experiences.
 
-    user_question = request.json.get('question')
-    os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
-    relevant_docs = knowledge_base.similarity_search(user_question, 3)
+                3. **Role Match**:
+                - For each role, provide:
+                    - The role name.
+                    - Identify a list of relevant skills from candidate's CV that directly fit the role (5 maximum).
+                    - If there are no relevant skills, indicate with a single bullet:
+                        * There are no skills that fit the job position.
+                    - A match score from 0 to 100 indicating how well the candidate fits the role.
+                    
+                \n{format_instructions}
+            """,
+        )
 
-    # Generate the answer using the preinitialized QA chain
-    answer = qa_chain.run(input_documents=relevant_docs, question=user_question)
+        chain = new_match_prompt | llm | feedback_parser
+        res = chain.invoke(input={"documents": documents, "roles": roles})
 
-    return jsonify({'answer': answer})
+        res_dict = res.to_dict()
 
-if __name__ == '__main__':
+        return jsonify(res_dict)
+
+if __name__ == "__main__":
     app.run(port=5000)
---
+++ output_parsers.py
@@ -0,0 +1,20 @@
+from typing import List, Dict, Any
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+class RoleMatch(BaseModel):
+    rol_name: str = Field(description="Name of the role")
+    fit_skills: List[str] = Field(description="Skills from the candidate that fit the role. Empty array if there are no roles")
+    match_score: int = Field(description="Score from 0 to 100 indicating how the candidate fits the role")
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {"rol_name": self.rol_name, "fit_skills": self.fit_skills, "match_score": self.match_score}
+
+class FeedbackModel(BaseModel):
+    main_skills: List[str] = Field(description="Main skills from the candidate CV")
+    role_matches: List[RoleMatch] = Field(description="List of role matches with the candidate")
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {"main_skills": self.main_skills, "role_matches": [role_match.to_dict() for role_match in self.role_matches]}
+
+feedback_parser = PydanticOutputParser(pydantic_object=FeedbackModel)
