9bc2c71366c90f1e41cf422b207633a6d0d38d23
Javier Candia <javi-candia@hotmail.com>
2024-12-05 16:24:49 -0600

    Added name of the candidate and short summary.

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ def upload_pdf():
                 Please perform the following tasks:
 
                 1. *Understand the Candidate CV*
+                - Give me the name of the candidate.
                 - Determine the candidate's level based on their overall experience: Junior, Mid or Senior.
 
                 2. **List the Candidate's Main Skills and Experiences**:
@@ -66,6 +67,7 @@ def upload_pdf():
                 3. **Role Match**:
                 - For each role, provide:
                     - The role name.
+                    - A short summary of the role description.
                     - Identify a list of relevant skills from candidate's CV that directly fit the role (5 maximum).
                     - If there are no relevant skills, indicate with a single bullet:
                         * There are no skills that fit the job position.
--- output_parsers.py
+++ output_parsers.py
@@ -3,18 +3,33 @@ from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 
 class RoleMatch(BaseModel):
-    rol_name: str = Field(description="Name of the role")
+    role_name: str = Field(description="Name of the role")
+    role_description: str = Field(description="Short summary of the role descirption")
     fit_skills: List[str] = Field(description="Skills from the candidate that fit the role. Empty array if there are no roles")
     match_score: int = Field(description="Score from 0 to 100 indicating how the candidate fits the role. 0 if no skills related")
 
     def to_dict(self) -> Dict[str, Any]:
-        return {"rol_name": self.rol_name, "fit_skills": self.fit_skills, "match_score": self.match_score}
+        return {
+            "role_name": self.role_name, 
+            "role_description": self.role_description,
+            "fit_skills": self.fit_skills, 
+            "match_score": self.match_score
+        }
 
 class FeedbackModel(BaseModel):
+    candidate_name: str = Field(description="Name of the candidate")
+    # experience_years: int = Field(description="Years of experience of the candidate")
+    candidate_level: str = Field(description="Candidate level: Junior, Mid or Senior")
     main_skills: List[str] = Field(description="Main skills from the candidate CV")
     role_matches: List[RoleMatch] = Field(description="List of role matches with the candidate")
 
     def to_dict(self) -> Dict[str, Any]:
-        return {"main_skills": self.main_skills, "role_matches": [role_match.to_dict() for role_match in self.role_matches]}
+        return {
+            "candidate_name": self.candidate_name,
+            # "experience_years": self.experience_years,
+            "candidate_level": self.candidate_level,
+            "main_skills": self.main_skills, 
+            "role_matches": [role_match.to_dict() for role_match in self.role_matches]
+        }
 
 feedback_parser = PydanticOutputParser(pydantic_object=FeedbackModel)
