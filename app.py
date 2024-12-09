59ab69f0902094898259a5acc8e8c1fa4716475c
Javier Candia <javi-candia@hotmail.com>
2024-12-09 17:31:51 -0600

    Added list of companies and start date of the rol to the response

--- app.py
+++ app.py
@@ -57,14 +57,17 @@ def upload_pdf():
 
                 Please perform the following tasks:
 
-                1. *Understand the Candidate CV*
+                1. **Understand the Candidate CV**
                 - Give me the name of the candidate.
                 - Determine the candidate's level based on their overall experience: Junior, Mid or Senior.
 
                 2. **List the Candidate's Main Skills and Experiences**:
                 - Identify and enumerate the 5 primary skills / technologies.
 
-                3. **Role Match**:
+                3. *List the companies where the candidate has worked for*
+                - Identify all the companies where the candidate has worked.
+
+                4. **Role Match**:
                 - For each role, provide:
                     - The role name.
                     - A short summary of the role description, 3 lines maximum.
@@ -72,6 +75,7 @@ def upload_pdf():
                     - If there are no relevant skills, indicate with a single bullet:
                         * There are no skills that fit the job position.
                     - A match score from 0 to 100 indicating how well the candidate fits the role. 0 If there are no skills that fit.
+                    - Start date of the role.
                     
                 \n{format_instructions}
             """,
--- output_parsers.py
+++ output_parsers.py
@@ -6,6 +6,7 @@ class RoleMatch(BaseModel):
     role_name: str = Field(description="Name of the role")
     role_description: str = Field(description="Short summary of the role descirption")
     fit_skills: List[str] = Field(description="Skills from the candidate that fit the role. Empty array if there are no roles")
+    start_date: str = Field(description="Role start date")
     match_score: int = Field(description="Score from 0 to 100 indicating how the candidate fits the role")
 
     def to_dict(self) -> Dict[str, Any]:
@@ -13,12 +14,14 @@ class RoleMatch(BaseModel):
             "role_name": self.role_name, 
             "role_description": self.role_description,
             "fit_skills": self.fit_skills, 
+            "start_date": self.start_date,
             "match_score": self.match_score
         }
 
 class FeedbackModel(BaseModel):
     candidate_name: str = Field(description="Name of the candidate")
     # experience_years: int = Field(description="Years of experience of the candidate")
+    companies: List[str] = Field(description="Companies where the candidate has worked")
     candidate_level: str = Field(description="Candidate level")
     main_skills: List[str] = Field(description="Main skills from the candidate CV")
     role_matches: List[RoleMatch] = Field(description="List of role matches with the candidate")
@@ -27,6 +30,7 @@ class FeedbackModel(BaseModel):
         return {
             "candidate_name": self.candidate_name,
             # "experience_years": self.experience_years,
+            "companies": self.companies,
             "candidate_level": self.candidate_level,
             "main_skills": self.main_skills, 
             "role_matches": [role_match.to_dict() for role_match in self.role_matches]
